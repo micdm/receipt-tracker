04dd9f043927e7dc0cf3308980e7cf9332e6da31
mic <demerzov@gmail.com>
2017-06-21 22:51:20 +0700

    fix: проще же можно регистрировать модели для админки

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,22 +1,8 @@
 from django.contrib import admin
 from main import models
 
-
-@admin.register(models.Seller)
-class SellerAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Receipt)
-class ReceiptAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.ReceiptItem)
-class ReceiptItemAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(models.Seller)
+admin.site.register(models.Product)
+admin.site.register(models.ProductAlias)
+admin.site.register(models.Receipt)
+admin.site.register(models.ReceiptItem)
