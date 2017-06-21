f0bf4dfe913df4992e1fd6491c90d849e54577e4
root <root@localhost>
2017-06-21 15:27:56 +0000

    new: настройки статики + игнорируем служебные директории

--- src/main/settings.py
+++ src/main/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/s/'
+STATIC_ROOT = os.path.join('..', 'static')
 
 LOGIN_URL = '/'
 
