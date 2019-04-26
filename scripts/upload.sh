e58663c59638412eca406f114b1015f79f8a1a89
mic <demerzov@gmail.com>
2019-04-26 22:47:08 +0700

    адрес сайта пусть передаётся через переменную

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-COOKIE="${1}"
-QR_CODE="${2}"
+SITE_ADDRESS="${1}"
+COOKIE="${2}"
+QR_CODE="${3}"
 
-STATUS_CODE=$(echo "${QR_CODE}" | curl -s -o /dev/null -w "%{http_code}" -H "Cookie: sessionid=${COOKIE}" -F 'text=<-' 'http://localhost:8000/receipts/add/')
+STATUS_CODE=$(echo "${QR_CODE}" | curl -s -o /dev/null -w "%{http_code}" -H "Cookie: sessionid=${COOKIE}" -F 'text=<-' "${SITE_ADDRESS}/receipts/add/")
 
 if [[ "${STATUS_CODE}" != 302 ]]; then
     echo "Cannot add ${QR_CODE}, status is ${STATUS_CODE}"
